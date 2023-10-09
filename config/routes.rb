8e1b7171ed718b2f2d72fadf6edecb3d634e39f4
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-09 21:53:56 +0700

    add route for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
+  
   get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
   devise_for :users
   
   authenticated :user do
     root 'public_recipes#index', as: :authenticated_root
   end
-
+  
   unauthenticated do
     root 'public_recipes#splash', as: :unauthenticated_root
   end
-
+  
   resources :public_recipes, only: %i[index]
+  
+  resources :foods, only: %i[index new create destroy]
+  resources :recipes, except: :edit do
+    resources :recipe_foods, only: %i[new create destroy]
+  end 
+
+  resources :shopping_lists, only: %i[index]
 end
