340ef64dbbe0a0408f6c4052fc6bcf686820dabc
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-10 06:09:43 +0700

    Styling new recipe view and controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,11 +6,34 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    @recipe.preparation_time = calculate_minutes(params[:recipe][:preparation_time_hr], params[:recipe][:preparation_time_min])
+    @recipe.cooking_time = calculate_minutes(params[:recipe][:cooking_time_hr], params[:recipe][:cooking_time_min])
+
+    if @recipe.save
+      # Handle successful save
+      redirect_to @recipe
+    else
+      # Handle validation errors
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :public, :preparation_time_hr, :preparation_time_min, :cooking_time_hr, :cooking_time_min)
+  end
+
+  def calculate_minutes(hours, minutes)
+    hours.to_i * 60 + minutes.to_i
   end
 end
