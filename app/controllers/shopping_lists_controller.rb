337542f965a4f49defcea07d35e4c2cd095908dc
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-10 23:31:03 +0700

    Create a shopping list based on recipe

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,6 +1,19 @@
 class ShoppingListsController < ApplicationController
   def index
-    @recipe_foods = RecipeFood.all
-    @total_value = @recipe_foods.sum(&:total_price)
+    recipe_id = params[:recipe_id]
+    if recipe_id.present?
+      @recipe = Recipe.find(recipe_id)
+
+      @recipe_foods = @recipe.recipe_foods
+      @total_value = @recipe_foods.sum(&:total_price)
+    end
+    # @recipe_foods = RecipeFood.all
+    # @total_value = @recipe_foods.sum(&:total_price)
+  end
+
+  def generate
+    recipe_id = params[:recipe_id]
+
+    redirect_to shopping_lists_path(recipe_id: recipe_id) 
   end
 end
