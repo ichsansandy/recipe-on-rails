312ba6fb6ff1fd8be3857c6045920c68fd7b5216
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-10 19:43:48 +0630

    Fix rubocop errors

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,8 +1,6 @@
 class ShoppingListsController < ApplicationController
   def index
-    def index
-      @recipe_foods = RecipeFood.all
-      @total_value = @recipe_foods.sum(&:total_price)
-    end
+    @recipe_foods = RecipeFood.all
+    @total_value = @recipe_foods.sum(&:total_price)
   end
 end
