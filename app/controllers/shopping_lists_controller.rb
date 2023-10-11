f9eb9e7ca559c52693f0dc620b0c999dbae6cc47
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-11 16:09:45 +0700

    fix linter

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,12 +1,13 @@
 class ShoppingListsController < ApplicationController
   def index
     recipe_id = params[:recipe_id]
-    if recipe_id.present?
-      @recipe = Recipe.find(recipe_id)
+    return unless recipe_id.present?
 
-      @recipe_foods = @recipe.recipe_foods
-      @total_value = @recipe_foods.sum(&:total_price)
-    end
+    @recipe = Recipe.find(recipe_id)
+
+    @recipe_foods = @recipe.recipe_foods
+    @total_value = @recipe_foods.sum(&:total_price)
+
     # @recipe_foods = RecipeFood.all
     # @total_value = @recipe_foods.sum(&:total_price)
   end
@@ -14,6 +15,6 @@ class ShoppingListsController < ApplicationController
   def generate
     recipe_id = params[:recipe_id]
 
-    redirect_to shopping_lists_path(recipe_id: recipe_id) 
+    redirect_to shopping_lists_path(recipe_id:)
   end
 end
